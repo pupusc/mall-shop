c41eee42a42df856c04974ca67a391a72b62cc0c
duanlongshan <duanlongshan@dushu365.com>
2021-09-10 18:27:13 +0800

    update

--- service-goods/service-goods-api/src/main/java/com/wanmi/sbc/goods/api/response/goods/GoodsListCustomProviderResponse.java
+++ service-goods/service-goods-api/src/main/java/com/wanmi/sbc/goods/api/response/goods/GoodsListCustomProviderResponse.java
@@ -1,6 +1,11 @@
 package com.wanmi.sbc.goods.api.response.goods;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * Description:
@@ -26,6 +31,51 @@ public class GoodsListCustomProviderResponse {
      */
     private String goodsSubName;
 
+    /**
+     * 上架状态 0 未上架  1 已经上架 2 部分上架 {@link com.wanmi.sbc.goods.bean.enums.AddedFlag}
+     */
+//    private Integer addedFlag;
+    private Integer forSaleStatus;
 
+//    /**
+//     * 上架时间
+//     */
+//    private LocalDateTime forSaleTime;
+
+    /**
+     * 0：待审核 1：已审核 2：审核失败 3：禁售中 {@link com.wanmi.sbc.goods.bean.enums.CheckStatus}
+     */
+//    private Integer checkStatus;
+    private Integer goodsStatus;
+
+    /**
+     * 积分
+     */
+    private Integer buyPoint;
+
+    /**
+     * 知识顾问专享 0:不是 ，1：是
+     */
+    private Integer cpsSpecial;
+
+    /**
+     * 商品创建时间
+     */
+    private LocalDateTime createTime;
+
+//    /**
+//     * 禁售状态,0:未禁售 1:禁售中
+//     */
+//    private Integer forbidStatus;
+
+    /**
+     * 排序的价格
+     */
+    private BigDecimal esSortPrice;
+
+    /**
+     * 划线价格
+     */
+    private BigDecimal linePrice;
 
 }
