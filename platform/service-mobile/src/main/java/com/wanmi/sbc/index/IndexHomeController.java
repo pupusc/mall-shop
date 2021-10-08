cedee23891faa79c14e8d5874ab8d3fd987bc47d
tangqinjing <tangqinjing@dushu365.com>
2021-10-08 18:45:44 +0800

    商城首页

--- platform/service-mobile/src/main/java/com/wanmi/sbc/index/IndexHomeController.java
+++ platform/service-mobile/src/main/java/com/wanmi/sbc/index/IndexHomeController.java
@@ -75,6 +75,7 @@ public class IndexHomeController {
         Long refreshHotCount;
         String ip = HttpUtil.getIpAddr();
         if (versionRequest.getPageNum() > 1) {
+            //翻页浏览，获取用户缓存浏览队列，若过期，取最新队列
             Object refresObject = redisTemplate.opsForValue().get("ip:" + ip);
             if (refresObject != null) {
                 refreshHotCount = Long.valueOf(refresObject.toString());
@@ -86,6 +87,7 @@ public class IndexHomeController {
                 redisTemplate.opsForValue().set("ip:" + ip, refreshHotCount, 30, TimeUnit.MINUTES);
             }
         } else {
+            //首页浏览，获取缓存最新队列，缓存用户浏览队列
             refreshHotCount = Long.valueOf(redisTemplate.opsForValue().get("refreshHotCount").toString());
             if (!redisTemplate.hasKey("hotGoods" + refreshHotCount) && !redisTemplate.hasKey("hotBooks" + refreshHotCount)) {
                 refreshHotCount = refreshHotCount - 1;
