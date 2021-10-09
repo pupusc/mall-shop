d87e01d7d5d1235c288e2b6cf11da5aaca1f8096
tangqinjing <tangqinjing@dushu365.com>
2021-10-09 11:22:49 +0800

    商城首页

--- service-goods/service-goods-core/src/main/java/com/wanmi/sbc/goods/hotgoods/repository/HotGoodsRepository.java
+++ service-goods/service-goods-core/src/main/java/com/wanmi/sbc/goods/hotgoods/repository/HotGoodsRepository.java
@@ -23,13 +23,13 @@ public interface HotGoodsRepository extends JpaRepository<HotGoods, String>,
      * @return
      */
     @Modifying
-    @Query("UPDATE HotGoods set sort = CEILING(RAND()*9000+1000 )")
+    @Query("UPDATE HotGoods set sort = CEILING(RAND()*9000+1000) where isRefresh = 1 ")
     int updateSort();
 
     /**
      * 获取所有数据并排序
      * @return
      */
-    @Query("from HotGoods where isRefresh = 1 order by sort desc ")
+    @Query("from HotGoods order by sort desc ")
     List<HotGoods> selectAllBySort();
 }
