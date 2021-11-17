71686ca58ee11a475efebc04a9f13d3c991f5b13
liuxia <liuxia@dushu365.com>
2021-11-17 10:19:36 +0800

    背景色

--- service-setting/service-setting-bean/src/main/java/com/wanmi/sbc/setting/bean/vo/TopicActivityVO.java
+++ service-setting/service-setting-bean/src/main/java/com/wanmi/sbc/setting/bean/vo/TopicActivityVO.java
@@ -24,7 +24,9 @@ public class TopicActivityVO implements Serializable {
     @ApiModelProperty("专题名称")
     private String topicName;
 
-    @ApiModelProperty("背景色")
-    private String background;
+    @ApiModelProperty("主题色")
+    private String topicColor;
 
+    @ApiModelProperty("导航色")
+    private String navigationColor;
 }
