76eaf9ae7088089c5c07869c6a8dbd24d3c817f0
liuxia <liuxia@dushu365.com>
2021-11-30 10:49:32 +0800

    登陆态

--- wm-micro-service/src/main/java/com/wanmi/ms/jwt/JwtInterceptor.java
+++ wm-micro-service/src/main/java/com/wanmi/ms/jwt/JwtInterceptor.java
@@ -105,6 +105,7 @@ public class JwtInterceptor implements WebRequestInterceptor {
                     || requestURI.startsWith("/index/hot")
                     || requestURI.startsWith("/index/shopActivityBranchHot")
                     || requestURI.startsWith("/index/shopActivityBranchConfig")
+                    || requestURI.startsWith("/topic/detail")
             ) {
                 if (StringUtils.isBlank(token)) {
                     log.info("JwtInterceptor preHandle out ['{} 当前token为空，直接非登陆情况下访问']", requestURI);
