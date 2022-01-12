89ec35f21124d55797f4c52a23beb1f495f76065
duanlongshan <duanlongshan@dushu365.com>
2022-01-12 17:07:24 +0800

    add log

--- service-order/service-order-core/src/main/java/com/wanmi/sbc/order/returnorder/fsm/action/RefundReturnAction.java
+++ service-order/service-order-core/src/main/java/com/wanmi/sbc/order/returnorder/fsm/action/RefundReturnAction.java
@@ -1,4 +1,6 @@
 package com.wanmi.sbc.order.returnorder.fsm.action;
+import com.wanmi.sbc.common.enums.DeleteFlag;
+import com.wanmi.sbc.order.bean.enums.HandleStatus;
 
 import com.wanmi.sbc.common.base.Operator;
 import com.wanmi.sbc.customer.api.provider.fandeng.ExternalProvider;
@@ -16,6 +18,8 @@ import com.wanmi.sbc.goods.api.request.storetobeevaluate.StoreTobeEvaluateListRe
 import com.wanmi.sbc.goods.bean.vo.GoodsTobeEvaluateVO;
 import com.wanmi.sbc.goods.bean.vo.StoreTobeEvaluateVO;
 import com.wanmi.sbc.order.bean.enums.ReturnFlowState;
+import com.wanmi.sbc.order.exceptionoftradepoints.model.root.ExceptionOfTradePoints;
+import com.wanmi.sbc.order.exceptionoftradepoints.service.ExceptionOfTradePointsService;
 import com.wanmi.sbc.order.returnorder.fsm.ReturnAction;
 import com.wanmi.sbc.order.returnorder.fsm.ReturnStateContext;
 import com.wanmi.sbc.order.returnorder.fsm.event.ReturnEvent;
@@ -57,8 +61,10 @@ public class RefundReturnAction extends ReturnAction {
     private StoreTobeEvaluateQueryProvider storeTobeEvaluateQueryProvider;
 
     @Autowired
-    private ExternalProvider externalProvider;
+    private ExceptionOfTradePointsService exceptionOfTradePointsService;
 
+    @Autowired
+    private ExternalProvider externalProvider;
 
     @Autowired
     private TradeRepository tradeRepository;
@@ -78,9 +84,19 @@ public class RefundReturnAction extends ReturnAction {
         returnOrderService.updateReturnOrder(returnOrder);
         super.operationLogMq.convertAndSend(operator, ReturnEvent.REFUND.getDesc(), eventLog.getEventDetail());
 
+
+
         //此处为退换积分和知豆，如果没有返还用户会找过来,后续添加日志表
-        Long points = Objects.nonNull(returnOrder.getReturnPoints()) ? returnOrder.getReturnPoints().getApplyPoints() : null;
-        if (points != null && points > 0) {
+        Long points = Objects.nonNull(returnOrder.getReturnPoints()) ? returnOrder.getReturnPoints().getApplyPoints() : 0L;
+        ExceptionOfTradePoints exceptionOfTradePoints = new ExceptionOfTradePoints();
+        exceptionOfTradePoints.setTradeId(returnOrder.getTid());
+        exceptionOfTradePoints.setPoints(points);
+        exceptionOfTradePoints.setHandleStatus(HandleStatus.PENDING);
+        exceptionOfTradePoints.setErrorTime(3); //超过3次不再处理
+        exceptionOfTradePoints.setDelFlag(DeleteFlag.NO);
+        exceptionOfTradePoints.setCreateTime(LocalDateTime.now());
+
+        if (points > 0) {
     /*        customerPointsDetailSaveProvider.returnPoints(CustomerPointsDetailAddRequest.builder()
                     .customerId(returnOrder.getBuyer().getId())
                     .type(OperateType.GROWTH)
@@ -88,25 +104,38 @@ public class RefundReturnAction extends ReturnAction {
                     .points(points)
                     .content(JSONObject.toJSONString(Collections.singletonMap("returnOrderNo", returnOrder.getId())))
                     .build());*/
+
+            exceptionOfTradePoints.setType(3); //不影响原来的类型内容,只是做记录
+            ExceptionOfTradePoints exceptionOfTradePointsModel = exceptionOfTradePointsService.add(exceptionOfTradePoints);
             try {
+
                 FanDengPointRefundRequest refundRequest = FanDengPointRefundRequest.builder()
                         .point(points).userNo(returnOrder.getFanDengUserNo()).sourceId(returnOrder.getId()).sourceType(1)
                         .desc("退单返还(退单号:"+returnOrder.getId()+")").build();
                 externalProvider.pointRefund(refundRequest);
+
+                exceptionOfTradePointsModel.setHandleStatus(HandleStatus.SUCCESSFULLY_PROCESSED);
+                exceptionOfTradePointsService.modify(exceptionOfTradePointsModel);
             } catch (Exception ex) {
                 String errorMsg = "RefundReturnAction 退单号:" + returnOrder.getId() +" 订单号:" + returnOrder.getTid() + " 退还积分执行异常";
                 log.error(errorMsg, ex);
             }
         }
         Long knowledge = Objects.nonNull(returnOrder.getReturnKnowledge()) ? returnOrder.getReturnKnowledge().getApplyKnowledge() : null;
-        if (knowledge != null && knowledge > 0) {
+        if (knowledge > 0) {
             try {
+                exceptionOfTradePoints.setType(4); //不影响原来的类型内容,只是做记录
+                ExceptionOfTradePoints exceptionOfTradePointsModel = exceptionOfTradePointsService.add(exceptionOfTradePoints);
+
                 Trade trade = tradeRepository.findById(returnOrder.getTid()).get();
                 FanDengKnowledgeRefundRequest refundRequest = FanDengKnowledgeRefundRequest.builder()
                         .beans(knowledge).userNo(returnOrder.getFanDengUserNo()).sourceId(returnOrder.getId())
                         .deductCode(trade.getDeductCode())
                         .desc("退单返还(退单号:"+returnOrder.getId()+")").build();
                 externalProvider.knowledgeRefund(refundRequest);
+
+                exceptionOfTradePointsModel.setHandleStatus(HandleStatus.SUCCESSFULLY_PROCESSED);
+                exceptionOfTradePointsService.modify(exceptionOfTradePointsModel);
             } catch (Exception ex) {
                 String errorMsg = "RefundReturnAction 退单号:" + returnOrder.getId() +" 订单号:" + returnOrder.getTid() + " 退还知豆执行异常";
                 log.error(errorMsg, ex);
