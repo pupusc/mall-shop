9de0e3d10cdeeda9d54b64a4a867c79afee28244
liangjun <liangjun@dushu365.com>
2022-02-21 14:31:41 +0800

    Update JwtInterceptor.java

--- common-config/src/main/java/com/wanmi/ms/jwt/JwtInterceptor.java
+++ common-config/src/main/java/com/wanmi/ms/jwt/JwtInterceptor.java
@@ -108,6 +108,8 @@ public class JwtInterceptor implements WebRequestInterceptor {
                     || requestURI.startsWith("/topic/detail")
                     || requestURI.startsWith("/topic/headTopic")
                     || requestURI.startsWith("/vote")
+                    || requestURI.startsWith("/error")
+                    || requestURI.startsWith("/")
             ) {
                 if (StringUtils.isBlank(token)) {
                     log.info("JwtInterceptor preHandle out ['{} 当前token为空，直接非登陆情况下访问']", requestURI);
