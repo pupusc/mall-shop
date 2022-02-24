a87c295517629fdfb8f1d1a19b79614afc14f172
wanghao <>
2022-02-24 11:28:09 +0800

    微信小程序

--- service-customer/service-customer-api/src/main/java/com/wanmi/sbc/customer/api/request/fandeng/FanDengWxAuthLoginRequest.java
+++ service-customer/service-customer-api/src/main/java/com/wanmi/sbc/customer/api/request/fandeng/FanDengWxAuthLoginRequest.java
@@ -1,10 +1,14 @@
 package com.wanmi.sbc.customer.api.request.fandeng;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FanDengWxAuthLoginRequest {
 
     private String unionId;
