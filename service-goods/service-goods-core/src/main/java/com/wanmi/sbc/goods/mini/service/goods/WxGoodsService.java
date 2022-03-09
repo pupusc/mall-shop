0e5914bf54a7fcf1037c587c9a1ed96f26e5c06e
wanghao <>
2022-03-09 18:13:28 +0800

    update

--- service-goods/service-goods-core/src/main/java/com/wanmi/sbc/goods/mini/service/goods/WxGoodsService.java
+++ service-goods/service-goods-core/src/main/java/com/wanmi/sbc/goods/mini/service/goods/WxGoodsService.java
@@ -239,6 +239,8 @@ public class WxGoodsService {
         return onShelfCount > 0;
     }
 
+
+    
     public WxAddProductRequest createWxAddProductRequestByGoods(Goods goods, List<GoodsInfo> goodsInfos, WxGoodsModel wxGoodsModel){
         WxAddProductRequest addProductRequest = new WxAddProductRequest();
         addProductRequest.setOutProductId(goods.getGoodsId());
