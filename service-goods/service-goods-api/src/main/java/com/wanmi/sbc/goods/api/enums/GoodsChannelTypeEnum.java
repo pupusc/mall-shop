0d4ac31661aa3a151fdaf06f740295730902764d
duanlongshan <duanlongshan@dushu365.com>
2022-03-18 02:29:47 +0800

    update

--- service-goods/service-goods-api/src/main/java/com/wanmi/sbc/goods/api/enums/GoodsChannelTypeEnum.java
+++ service-goods/service-goods-api/src/main/java/com/wanmi/sbc/goods/api/enums/GoodsChannelTypeEnum.java
@@ -1,30 +1,30 @@
-//package com.wanmi.sbc.goods.api.enums;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//
-//import java.util.Objects;
-//
-///**
-// * 商品销售平台
-// */
-//@Getter
-//@AllArgsConstructor
-//public enum GoodsChannelTypeEnum {
-//    MALL_H5("1", "商城-H5"),
-//    MALL_MINI("2", "商城-小程序"),
-//    MALL_NORMAL("3", "商城-普通分类"),
-//    FDDS_DELIVER("4", "樊登读书-实物履约");
-//
-//    private final String code;
-//    private final String desc;
-//
-//    public static GoodsChannelTypeEnum getByCode(int code) {
-//        for (GoodsChannelTypeEnum p : values()) {
-//            if (Objects.equals(p.getCode(), code)) {
-//                return p;
-//            }
-//        }
-//        return null;
-//    }
-//}
+package com.wanmi.sbc.goods.api.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+
+/**
+ * 商品销售平台
+ */
+@Getter
+@AllArgsConstructor
+public enum GoodsChannelTypeEnum {
+    MALL_H5("1", "商城-H5"),
+    MALL_MINI("2", "商城-小程序"),
+    MALL_NORMAL("3", "商城-普通分类"),
+    FDDS_DELIVER("4", "樊登读书-实物履约");
+
+    private final String code;
+    private final String desc;
+
+    public static GoodsChannelTypeEnum getByCode(int code) {
+        for (GoodsChannelTypeEnum p : values()) {
+            if (Objects.equals(p.getCode(), code)) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
