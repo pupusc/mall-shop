3f38077c3afbb42f96648ef0da47a1475daed8e9
duanlongshan <duanlongshan@dushu365.com>
2022-03-22 16:58:20 +0800

    add log

--- service-order/service-order-core/src/main/java/com/wanmi/sbc/order/trade/service/ProviderTradeService.java
+++ service-order/service-order-core/src/main/java/com/wanmi/sbc/order/trade/service/ProviderTradeService.java
@@ -1218,6 +1218,7 @@ public class ProviderTradeService {
 
             if (CollectionUtils.isNotEmpty(providerTrades)) {
                 providerTrades.forEach(providerTrade -> {
+                    log.info("erpResetStatusSyncCountJobHandler ProviderTradeService tid: {} pid:{}", providerTrade.getParentId(), providerTrade.getId());
                     providerTrade.getTradeState().setScanCount(ScanCount.COUNT_ZERO.toValue());
                 });
                 this.updateProviderTradeList(providerTrades);
