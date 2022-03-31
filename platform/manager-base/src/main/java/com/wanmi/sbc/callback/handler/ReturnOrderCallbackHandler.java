8765fc75efb75ac3f28776cd86e9d4e0f6914baf
duanlongshan <duanlongshan@dushu365.com>
2022-03-31 19:56:54 +0800

    update 微信小程序售后

--- platform/manager-base/src/main/java/com/wanmi/sbc/callback/handler/ReturnOrderCallbackHandler.java
+++ platform/manager-base/src/main/java/com/wanmi/sbc/callback/handler/ReturnOrderCallbackHandler.java
@@ -3,18 +3,32 @@ package com.wanmi.sbc.callback.handler;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.wanmi.sbc.common.base.BaseResponse;
+import com.wanmi.sbc.common.base.Operator;
+import com.wanmi.sbc.common.enums.Platform;
+import com.wanmi.sbc.common.util.HttpUtil;
 import com.wanmi.sbc.common.util.KsBeanUtil;
 import com.wanmi.sbc.order.api.provider.refund.RefundOrderQueryProvider;
 import com.wanmi.sbc.order.api.provider.returnorder.ReturnOrderProvider;
 import com.wanmi.sbc.order.api.provider.returnorder.ReturnOrderQueryProvider;
+import com.wanmi.sbc.order.api.provider.trade.TradeQueryProvider;
 import com.wanmi.sbc.order.api.request.refund.RefundOrderByReturnOrderCodeRequest;
+import com.wanmi.sbc.order.api.request.returnorder.ReturnOrderByConditionRequest;
 import com.wanmi.sbc.order.api.request.returnorder.ReturnOrderByIdRequest;
 import com.wanmi.sbc.order.api.request.returnorder.ReturnOrderOnlineRefundRequest;
+import com.wanmi.sbc.order.api.request.trade.TradeGetByIdRequest;
+import com.wanmi.sbc.order.api.response.refund.RefundOrderByReturnCodeResponse;
 import com.wanmi.sbc.order.api.response.returnorder.ReturnOrderByIdResponse;
+import com.wanmi.sbc.order.api.response.trade.TradeGetByIdResponse;
 import com.wanmi.sbc.order.bean.dto.RefundOrderDTO;
 import com.wanmi.sbc.order.bean.dto.ReturnOrderDTO;
+import com.wanmi.sbc.order.bean.enums.RefundChannel;
+import com.wanmi.sbc.order.bean.vo.ReturnOrderVO;
+import com.wanmi.sbc.order.bean.vo.TradeVO;
 import com.wanmi.sbc.pay.api.provider.PayProvider;
+import com.wanmi.sbc.pay.api.provider.PayQueryProvider;
 import com.wanmi.sbc.pay.api.request.PayTradeRecordRequest;
+import com.wanmi.sbc.pay.api.request.TradeRecordByOrderCodeRequest;
+import com.wanmi.sbc.pay.api.response.PayTradeRecordResponse;
 import com.wanmi.sbc.pay.weixinpaysdk.WXPayConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,10 +53,16 @@ public class ReturnOrderCallbackHandler implements CallbackHandler{
     private PayProvider payProvider;
 
     @Autowired
-    private ReturnOrderQueryProvider returnOrderQueryProvider;
+    private TradeQueryProvider tradeQueryProvider;
 
     @Autowired
     private ReturnOrderProvider returnOrderProvider;
+
+    @Autowired
+    private ReturnOrderQueryProvider returnOrderQueryProvider;
+
+    @Autowired
+    private RefundOrderQueryProvider refundOrderQueryProvider;
 
     @Override
     public boolean support(String eventType) {
@@ -51,38 +71,73 @@ public class ReturnOrderCallbackHandler implements CallbackHandler{
 
     @Override
     public void handle(Map<String, Object> paramMap) {
-
+        log.info("ReturnOrderCallbackHandler handle --> begin");
+        long beginTime = System.currentTimeMillis();
         Object returnOrderObj = paramMap.get("aftersale_info");
         if (returnOrderObj == null) {
             log.error("回调参数异常");
             return;
         }
         JSONObject returnOrderObjJson = JSON.parseObject(returnOrderObj.toString());
-        String returnOrderIdObj = returnOrderObjJson.getString("out_aftersale_id");
-        String returnOrderTradeNo = returnOrderObjJson.getString("aftersale_id");
+        String returnOrderId = returnOrderObjJson.getString("out_aftersale_id");
+        String returnOrderTradeNo = returnOrderObjJson.getString("aftersale_id"); //退单流水
 
+        log.info("ReturnOrderCallbackHandler handle out_aftersale_id: {}, aftersale_id: {}", returnOrderObj, returnOrderTradeNo);
+        ReturnOrderVO returnOrder = returnOrderQueryProvider.getById(ReturnOrderByIdRequest.builder()
+                    .rid(returnOrderId).build()).getContext();
 
-        //获取退单
-        ReturnOrderByIdRequest request = new ReturnOrderByIdRequest();
-        BaseResponse<ReturnOrderByIdResponse> returnOrderByIdResponseBaseResponse = returnOrderQueryProvider.getById(request);
-        ReturnOrderByIdResponse context = returnOrderByIdResponseBaseResponse.getContext();
-        if (context == null) {
-            return;
-        }
+        //获取订单信息
+        TradeGetByIdRequest tradeGetByIdRequest = new TradeGetByIdRequest();
+        tradeGetByIdRequest.setTid(returnOrder.getTid());
+        BaseResponse<TradeGetByIdResponse> tradeGetByIdResponse = tradeQueryProvider.getById(tradeGetByIdRequest);
+        TradeGetByIdResponse tradeGetById = tradeGetByIdResponse.getContext();
+        TradeVO tradeVO = tradeGetById.getTradeVO();
+
+        RefundOrderByReturnCodeResponse refundOrder =
+                refundOrderQueryProvider.getByReturnOrderCode(new RefundOrderByReturnOrderCodeRequest(returnOrder.getTid())).getContext();
+
+        Operator operator = Operator.builder().ip(HttpUtil.getIpAddr()).adminId("-1").name("UNIONB2B")
+                .platform(Platform.THIRD).build();
 
         PayTradeRecordRequest payTradeRecordRequest = new PayTradeRecordRequest();
         payTradeRecordRequest.setTradeNo(returnOrderTradeNo);
-        payTradeRecordRequest.setBusinessId(returnOrderIdObj);
+        payTradeRecordRequest.setBusinessId(returnOrderId);
         payTradeRecordRequest.setResult_code(WXPayConstants.SUCCESS);
-        BigDecimal applyPrice = context.getReturnPrice().getApplyPrice().
-                divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_DOWN));
-        payTradeRecordRequest.setApplyPrice(applyPrice);
-        payTradeRecordRequest.setPracticalPrice(applyPrice);
-        payProvider.wxPayCallBack(payTradeRecordRequest);
 
+        payTradeRecordRequest.setApplyPrice(returnOrder.getReturnPrice().getApplyPrice().divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_DOWN));
+        payTradeRecordRequest.setPracticalPrice(tradeVO.getTradePrice().getTotalPrice().divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_DOWN));
+        payProvider.wxPayCallBack(payTradeRecordRequest);
         returnOrderProvider.onlineRefund(
                 ReturnOrderOnlineRefundRequest.builder().operator(operator)
                         .returnOrder(KsBeanUtil.convert(returnOrder, ReturnOrderDTO.class))
                         .refundOrder(KsBeanUtil.convert(refundOrder, RefundOrderDTO.class)).build());
+
+        log.info("ReturnOrderCallbackHandler handle --> end cost: {} ms", System.currentTimeMillis() - beginTime);
+//
+//        //获取退单
+//        ReturnOrderByIdRequest request = new ReturnOrderByIdRequest();
+//        BaseResponse<ReturnOrderByIdResponse> returnOrderByIdResponseBaseResponse = returnOrderQueryProvider.getById(request);
+//        ReturnOrderByIdResponse context = returnOrderByIdResponseBaseResponse.getContext();
+//        if (context == null) {
+//            return;
+//        }
+//
+//        PayTradeRecordRequest payTradeRecordRequest = new PayTradeRecordRequest();
+//        payTradeRecordRequest.setTradeNo(returnOrderTradeNo);
+//        payTradeRecordRequest.setBusinessId(returnOrderIdObj);
+//        payTradeRecordRequest.setResult_code(WXPayConstants.SUCCESS);
+//        BigDecimal applyPrice = context.getReturnPrice().getApplyPrice().
+//                divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_DOWN));
+//        payTradeRecordRequest.setApplyPrice(applyPrice);
+//        payTradeRecordRequest.setPracticalPrice(applyPrice);
+//        payProvider.wxPayCallBack(payTradeRecordRequest);
+//
+//        Operator operator =
+//                Operator.builder().adminId("1").name("system").account("system").ip("127.0.0.1").platform(Platform.PLATFORM).build();
+//
+//        returnOrderProvider.onlineRefund(
+//                ReturnOrderOnlineRefundRequest.builder().operator(operator)
+//                        .returnOrder(KsBeanUtil.convert(returnOrder, ReturnOrderDTO.class))
+//                        .refundOrder(KsBeanUtil.convert(refundOrder, RefundOrderDTO.class)).build());
     }
 }
