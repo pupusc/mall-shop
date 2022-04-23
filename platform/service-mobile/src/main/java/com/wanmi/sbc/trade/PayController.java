1b664247e4e82e16efe6b579b07123e5caaf619d
duanlongshan <duanlongshan@dushu365.com>
2022-04-23 17:07:10 +0800

    更改提示信息

--- platform/service-mobile/src/main/java/com/wanmi/sbc/trade/PayController.java
+++ platform/service-mobile/src/main/java/com/wanmi/sbc/trade/PayController.java
@@ -508,26 +508,41 @@ public class PayController {
     @ApiImplicitParam(paramType = "path", dataType = "String", name = "tid", value = "订单号", required = true)
     @RequestMapping(value = "/weiXinPay/checkOrderPayState/{tid}", method = RequestMethod.GET)
     public BaseResponse checkOrderPayState(@PathVariable String tid) {
-        Boolean check = redisService.setNx("pay:" + tid, "1", 4L);
-        if(BooleanUtils.isFalse(check)){
-            throw new SbcRuntimeException("请稍后再试");
-        }
+        RLock lock = redissonClient.getLock(RedisKeyUtil.KEY_PAY_CHECK_LOCK + tid);
         String flag = "0";
-        List<TradeVO> tradeVOList = new ArrayList<>();
-        if (tid.startsWith(GeneratorService._PREFIX_TRADE_ID)) {
-            tradeVOList.add(tradeQueryProvider.getById(TradeGetByIdRequest.builder()
-                    .tid(tid).build()).getContext().getTradeVO());
-        } else if (tid.startsWith(GeneratorService._PREFIX_PARENT_TRADE_ID)) {
-            tradeVOList.addAll(tradeQueryProvider.getListByParentId(TradeListByParentIdRequest.builder().parentTid(tid)
-                    .build()).getContext().getTradeVOList());
-        } else {
-            throw new SbcRuntimeException(CommonErrorCode.PARAMETER_ERROR);
+        try {
+            if (lock.tryLock(3, 5, TimeUnit.SECONDS)) {
+                List<TradeVO> tradeVOList = new ArrayList<>();
+                if (tid.startsWith(GeneratorService._PREFIX_TRADE_ID)) {
+                    tradeVOList.add(tradeQueryProvider.getById(TradeGetByIdRequest.builder()
+                            .tid(tid).build()).getContext().getTradeVO());
+                } else if (tid.startsWith(GeneratorService._PREFIX_PARENT_TRADE_ID)) {
+                    tradeVOList.addAll(tradeQueryProvider.getListByParentId(TradeListByParentIdRequest.builder().parentTid(tid)
+                            .build()).getContext().getTradeVOList());
+                } else {
+                    throw new SbcRuntimeException(CommonErrorCode.PARAMETER_ERROR);
+                }
+                if (tradeVOList.size() > 0
+                        && (tradeVOList.get(0).getTradeState().getPayState() == PayState.PAID
+                        || tradeVOList.get(0).getTradeState().getPayState() == PayState.PAID_EARNEST)) {
+                    flag = "1";
+                }
+            } else {
+                throw new SbcRuntimeException("K-050512");
+            }
+        } catch (InterruptedException ex) {
+            log.error("PayController checkOrderPayState execute InterruptedException Exception", ex);
+            throw new SbcRuntimeException("K-000001");
+        } catch (Exception ex) {
+            log.error("PayController checkOrderPayState execute Exception", ex);
+            if (ex instanceof SbcRuntimeException) {
+                throw ex;
+            }
+            throw new SbcRuntimeException("K-000001");
+        } finally {
+            lock.unlock();
         }
-        if (tradeVOList.size() > 0
-                && (tradeVOList.get(0).getTradeState().getPayState() == PayState.PAID
-                || tradeVOList.get(0).getTradeState().getPayState() == PayState.PAID_EARNEST)) {
-            flag = "1";
-        }
+
         return BaseResponse.success(flag);
     }
 
