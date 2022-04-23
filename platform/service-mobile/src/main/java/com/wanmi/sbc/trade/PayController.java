72b03dbf300ecbc2a7cc079ea4395699d0143398
duanlongshan <duanlongshan@dushu365.com>
2022-04-23 17:03:03 +0800

    更改支付提示

--- platform/service-mobile/src/main/java/com/wanmi/sbc/trade/PayController.java
+++ platform/service-mobile/src/main/java/com/wanmi/sbc/trade/PayController.java
@@ -69,6 +69,7 @@ import com.wanmi.sbc.third.wechat.WechatSetService;
 import com.wanmi.sbc.trade.request.PayMobileRequest;
 import com.wanmi.sbc.trade.request.WeiXinPayRequest;
 import com.wanmi.sbc.util.CommonUtil;
+import com.wanmi.sbc.util.RedisKeyUtil;
 import io.seata.spring.annotation.GlobalTransactional;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -461,16 +462,21 @@ public class PayController {
     @ApiImplicitParam(paramType = "path", dataType = "String", name = "tid", value = "订单号", required = true)
     @RequestMapping(value = "/aliPay/check/{tid}", method = RequestMethod.GET)
     public BaseResponse checkPayState(@PathVariable String tid) {
-        Boolean check = redisService.setNx("pay:" + tid, "1", 4L);
-        if(BooleanUtils.isFalse(check)){
-            throw new SbcRuntimeException("请稍后再试");
-        }
-        TradeVO trade = tradeQueryProvider.getById(TradeGetByIdRequest.builder()
-                .tid(tid).build()).getContext().getTradeVO();
+        RLock lock = redissonClient.getLock(RedisKeyUtil.KEY_PAY_CHECK_LOCK + tid);
         //0:未支付
         String flag = "0";
-        if (Objects.nonNull(trade)) {
-            if (Objects.nonNull(trade.getTradeState())) {
+        try {
+            if (lock.tryLock(3, 5, TimeUnit.SECONDS)) {
+                TradeVO trade = tradeQueryProvider.getById(TradeGetByIdRequest.builder()
+                        .tid(tid).build()).getContext().getTradeVO();
+                if (trade == null){
+                    throw new SbcRuntimeException("K-020007");
+                }
+
+                if (trade.getTradeState() == null) {
+                    throw new SbcRuntimeException("K-050142");
+                }
+
                 LocalDateTime orderTimeOut = trade.getOrderTimeOut();
                 //已支付
                 if (PayState.PAID.equals(trade.getTradeState().getPayState())) {
@@ -480,7 +486,20 @@ public class PayController {
                 if (FlowState.VOID.equals(trade.getTradeState().getFlowState()) || Objects.nonNull(orderTimeOut) && orderTimeOut.isBefore(LocalDateTime.now())) {
                     flag = "2";
                 }
+            } else {
+                throw new SbcRuntimeException("K-050512");
             }
+        } catch (InterruptedException ex) {
+            log.error("PayController checkPayState execute InterruptedException Exception", ex);
+            throw new SbcRuntimeException("K-000001");
+        } catch (Exception ex) {
+            log.error("PayController checkPayState execute Exception", ex);
+            if (ex instanceof SbcRuntimeException) {
+                throw ex;
+            }
+            throw new SbcRuntimeException("K-000001");
+        } finally {
+            lock.unlock();
         }
         return BaseResponse.success(flag);
     }
