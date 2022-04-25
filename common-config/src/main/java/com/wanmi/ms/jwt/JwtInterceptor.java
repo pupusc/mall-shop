431fee01489aac4010364b1ea05db86e13f48eb5
liangjun <420333870@qq.com>
2022-04-25 21:52:54 +0800

    去掉回调的鉴权验证

--- common-config/src/main/java/com/wanmi/ms/jwt/JwtInterceptor.java
+++ common-config/src/main/java/com/wanmi/ms/jwt/JwtInterceptor.java
@@ -114,6 +114,7 @@ public class JwtInterceptor implements WebRequestInterceptor {
                     || requestURI.startsWith("/error")
                     || requestURI.startsWith("/wx/callback")
                     || requestURI.startsWith("/wx/order/purchase")
+                    || requestURI.startsWith("/invoice/callback")
             ) {
                 if (StringUtils.isBlank(token)) {
                     log.info("JwtInterceptor preHandle out ['{} 当前token为空，直接非登陆情况下访问']", requestURI);
