967fc891bb5be11af51f700e5c98b3ace50e2d3b
duanlongshan <duanlongshan@dushu365.com>
2022-05-24 17:03:32 +0800

    update time

--- service-goods/service-goods-core/src/main/java/com/wanmi/sbc/goods/mini/service/assistant/WxLiveAssistantService.java
+++ service-goods/service-goods-core/src/main/java/com/wanmi/sbc/goods/mini/service/assistant/WxLiveAssistantService.java
@@ -463,9 +463,9 @@ public class WxLiveAssistantService {
             throw new SbcRuntimeException(CommonErrorCode.SPECIFIED, "请先关闭同步，才能重新开启");
         }
 
-        LocalDateTime beforeDateTime = wxLiveAssistantModel.getStartTime().minusMonths(15);
+        LocalDateTime beforeDateTime = wxLiveAssistantModel.getStartTime().minusMonths(60);
         if (beforeDateTime.isBefore(LocalDateTime.now())) {
-            throw new SbcRuntimeException(CommonErrorCode.SPECIFIED, "直播开始时间前15分钟才可以开启");
+            throw new SbcRuntimeException(CommonErrorCode.SPECIFIED, "直播开始时间前1小时才可以开启");
         }
 
         /**
