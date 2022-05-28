d0d04566bca2717fba9b0ab481204e0808dd4d25
duanlongshan <duanlongshan@dushu365.com>
2022-05-29 01:35:55 +0800

    remove

--- service-elastic/service-elastic-app/src/main/java/com/wanmi/sbc/elastic/ElasticServiceApplication.java
+++ service-elastic/service-elastic-app/src/main/java/com/wanmi/sbc/elastic/ElasticServiceApplication.java
@@ -1,14 +1,12 @@
 package com.wanmi.sbc.elastic;
 
 
-import com.wanmi.sbc.common.config.redis.RedisAutoConfiguration;
 import com.wanmi.sbc.common.configure.CompositePropertySourceFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
