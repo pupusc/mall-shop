ffd3490cf08da2c8c530953ccfa9ad483123777f
duanlongshan <duanlongshan@dushu365.com>
2022-06-22 18:33:05 +0800

    快递信息

--- service-setting/service-setting-core/src/main/java/com/wanmi/sbc/setting/kuaidi100/DeliveryService.java
+++ service-setting/service-setting-core/src/main/java/com/wanmi/sbc/setting/kuaidi100/DeliveryService.java
@@ -19,6 +19,8 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -30,17 +32,20 @@ import java.util.*;
  */
 @Slf4j
 @Service
+@RefreshScope
 public class DeliveryService {
 
 
     @Autowired
     private ConfigService configService;
+    @Value("${openNewDelivery}")
+    private String newKuaiDi = "TRUE";
 
     /**
      * kuaidi100 请求地址
      */
-//    private static final String KUAIDI_URL = "http://poll.kuaidi100.com/poll/query.do";
-    private static final String KUAIDI_URL = "https://wuliu.market.alicloudapi.com/kdi";
+    private static final String KUAIDI_URL = "http://poll.kuaidi100.com/poll/query.do";
+    private static final String KUAIDI_NEW_URL = "https://wuliu.market.alicloudapi.com/kdi";
 
     /**
      * 自定义的物流查询
@@ -48,7 +53,7 @@ public class DeliveryService {
      * @return
      * @throws Exception
      */
-    public List<Map<Object, Object>> queryExpressInfoUrl(DeliveryQueryRequest queryRequest) throws Exception {
+    public List<Map<Object, Object>> queryNewExpressInfoUrl(DeliveryQueryRequest queryRequest) throws Exception {
         List<Map<Object, Object>> deliverLogisticsList = new ArrayList<>();
 
         if (StringUtils.isBlank(queryRequest.getDeliveryNo())) {
@@ -60,7 +65,7 @@ public class DeliveryService {
 
         Map<String, String> querys = new HashMap<>();
         querys.put("no", queryRequest.getDeliveryNo());
-        HttpResponse httpResponse = HttpUtil.doGet(KUAIDI_URL, "", "", headers, querys);
+        HttpResponse httpResponse = HttpUtil.doGet(KUAIDI_NEW_URL, "", "", headers, querys);
         if (httpResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
             throw new SbcRuntimeException("K-999999");
         }
@@ -99,48 +104,53 @@ public class DeliveryService {
      * @param queryRequest
      * @return
      */
-//    public List<Map<Object, Object>> queryExpressInfoUrl(DeliveryQueryRequest queryRequest) throws Exception {
-//        List<Map<Object, Object>> orderList = new ArrayList<Map<Object, Object>>();
-//
-//        //获取快递100的key
-//        ConfigQueryRequest request = new ConfigQueryRequest();
-//        request.setConfigType(ConfigType.KUAIDI100.toValue());
-//        LogisticsRopResponse response = configService.findKuaiDiConfig(request.getConfigType(), DeleteFlag.NO);
-//        if (Objects.isNull(response)) {
-//            throw new SbcRuntimeException(SiteResultCode.ERROR_000001);
-//        }
-//        String customer = response.getCustomerKey();
-//        String kuaidiKey = response.getDeliveryKey();
-//        //查询参数
-//        String param = "{\"com\":\"" + queryRequest.getCompanyCode() + "\",\"num\":\"" + queryRequest.getDeliveryNo() + "\"}";
-//        //加密的签名
-//        String sign = (MD5Util.md5Hex(param + kuaidiKey + customer, "utf-8")).toUpperCase();
-//        //查询所需的参数
-//        HashMap<String, String> params = new HashMap<String, String>();
-//        params.put("param", param);
-//        params.put("sign", sign);
-//        params.put("customer", customer);
-//
-//        String result = "";
-//        try {
-//            result = HttpRequest.postData(KUAIDI_URL, params, "utf-8").toString();
-//            // 格式化数据
-//            JSONObject reslut = JSONObject.parseObject(result);
-//            JSONArray kuaidiList = JSONArray.parseArray(reslut.get("data").toString());
-//            if (kuaidiList != null && kuaidiList.size() > 0) {
-//                for (int i = 0; i < kuaidiList.size(); i++) {
-//                    JSONObject jobj = JSON.parseObject(kuaidiList.get(i).toString(), JSONObject.class);
-//                    Map<Object, Object> map = new HashMap<Object, Object>();
-//                    map.put("time", jobj.get("ftime"));
-//                    map.put("context", jobj.get("context"));
-//                    orderList.add(map);
-//                }
-//            }
-//        } catch (Exception e) {
-//            log.error("调用失败-返回值：{}", result);
-//            throw e;
-//        }
-//
-//        return orderList;
-//    }
+    public List<Map<Object, Object>> queryExpressInfoUrl(DeliveryQueryRequest queryRequest) throws Exception {
+
+        if (Objects.equals(newKuaiDi, "TRUE")) {
+            return this.queryNewExpressInfoUrl(queryRequest);
+        }
+
+        List<Map<Object, Object>> orderList = new ArrayList<Map<Object, Object>>();
+
+        //获取快递100的key
+        ConfigQueryRequest request = new ConfigQueryRequest();
+        request.setConfigType(ConfigType.KUAIDI100.toValue());
+        LogisticsRopResponse response = configService.findKuaiDiConfig(request.getConfigType(), DeleteFlag.NO);
+        if (Objects.isNull(response)) {
+            throw new SbcRuntimeException(SiteResultCode.ERROR_000001);
+        }
+        String customer = response.getCustomerKey();
+        String kuaidiKey = response.getDeliveryKey();
+        //查询参数
+        String param = "{\"com\":\"" + queryRequest.getCompanyCode() + "\",\"num\":\"" + queryRequest.getDeliveryNo() + "\"}";
+        //加密的签名
+        String sign = (MD5Util.md5Hex(param + kuaidiKey + customer, "utf-8")).toUpperCase();
+        //查询所需的参数
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("param", param);
+        params.put("sign", sign);
+        params.put("customer", customer);
+
+        String result = "";
+        try {
+            result = HttpRequest.postData(KUAIDI_URL, params, "utf-8").toString();
+            // 格式化数据
+            JSONObject reslut = JSONObject.parseObject(result);
+            JSONArray kuaidiList = JSONArray.parseArray(reslut.get("data").toString());
+            if (kuaidiList != null && kuaidiList.size() > 0) {
+                for (int i = 0; i < kuaidiList.size(); i++) {
+                    JSONObject jobj = JSON.parseObject(kuaidiList.get(i).toString(), JSONObject.class);
+                    Map<Object, Object> map = new HashMap<Object, Object>();
+                    map.put("time", jobj.get("ftime"));
+                    map.put("context", jobj.get("context"));
+                    orderList.add(map);
+                }
+            }
+        } catch (Exception e) {
+            log.error("调用失败-返回值：{}", result);
+            throw e;
+        }
+
+        return orderList;
+    }
 }
