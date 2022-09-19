2ec705910e6f58e7b44eb6f9ab4dd2327efa2686
duanlongshan <duanlongshan@dushu365.com>
2022-09-20 02:09:08 +0800

    feat(商城) 改造

--- service-order/service-order-core/src/main/java/com/wanmi/sbc/order/open/model/FddsOrderCreateResult.java
+++ service-order/service-order-core/src/main/java/com/wanmi/sbc/order/open/model/FddsOrderCreateResult.java
@@ -1,24 +1,24 @@
-package com.wanmi.sbc.order.open.model;
-
-import lombok.Data;
-
-import java.io.*;
-
-/**
- * @author Liang Jun
- * @date 2022-04-02 14:36:00
- */
-@Data
-public class FddsOrderCreateResult extends FddsBaseResult<FddsOrderCreateResultData> implements Serializable {
-    /**
-     * status：状态
-     * 0000	操作成功
-     * 2001	会期商品不存在
-     * 2006	已领取不可重复领取
-     * 2007	商品已下架
-     * 2008	书籍包已下线或未发布
-     * 2009	书籍包重复购买
-     * 2010	结算方式未配置
-     * 2007	商品已下架
-     */
-}
+//package com.wanmi.sbc.order.open.model;
+//
+//import lombok.Data;
+//
+//import java.io.*;
+//
+///**
+// * @author Liang Jun
+// * @date 2022-04-02 14:36:00
+// */
+//@Data
+//public class FddsOrderCreateResult extends FddsBaseResult<FddsOrderCreateResultData> implements Serializable {
+//    /**
+//     * status：状态
+//     * 0000	操作成功
+//     * 2001	会期商品不存在
+//     * 2006	已领取不可重复领取
+//     * 2007	商品已下架
+//     * 2008	书籍包已下线或未发布
+//     * 2009	书籍包重复购买
+//     * 2010	结算方式未配置
+//     * 2007	商品已下架
+//     */
+//}
