07b77c06e0151e4d8646fb4c508ffa636bc29cb1
duanlongshan <duanlongshan@dushu365.com>
2022-11-09 18:09:06 +0800

    feat(积分运费) 订单结果上添加 运费金额信息

--- service-order/service-order-bean/src/main/java/com/wanmi/sbc/order/bean/dto/TradePriceDTO.java
+++ service-order/service-order-bean/src/main/java/com/wanmi/sbc/order/bean/dto/TradePriceDTO.java
@@ -35,6 +35,21 @@ public class TradePriceDTO implements Serializable {
     private BigDecimal deliveryPrice;
 
     /**
+     * 实际运费积分
+     */
+    private BigDecimal deliveryPointPrice;
+
+    /**
+     * 实际运费积分
+     */
+    private Long deliveryPoint;
+
+    /**
+     * 实际运费现金金额
+     */
+    private BigDecimal deliveryPayPrice;
+
+    /**
      * 特价金额，可以从TradePriceInfo获取
      */
     @ApiModelProperty(value = "特价金额")
