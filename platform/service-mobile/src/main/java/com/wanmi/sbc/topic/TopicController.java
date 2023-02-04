7fe290a20a0b07f6cacebd465e179f8edaf242c2
chenzhen <chenzhen@ditasys.com>
2023-02-04 14:50:59 +0800

    搜索前10

--- platform/service-mobile/src/main/java/com/wanmi/sbc/topic/TopicController.java
+++ platform/service-mobile/src/main/java/com/wanmi/sbc/topic/TopicController.java
@@ -48,5 +48,34 @@ public class TopicController {
     }
 
 
+    /**
+     * @description 根据专题id返回页面数据
+     * @menu 专题
+     * @param request
+     * @status undone
+     */
+    @ApiOperation(value = "根据专题id返回页面数据，")
+    @PostMapping(value = "/v2/detail")
+    public BaseResponse<TopicResponse> detailV2(@RequestBody TopicQueryRequest request) {
+        BaseResponse<TopicResponse> response = topicService.detail(request,true);
+        return response;
+    }
+
+    @ApiOperation(value = "根据专题id返回数据，第一次加载只返回1，2楼层数据信息")
+    @PostMapping(value = "/v2/headTopic")
+    public BaseResponse<TopicResponse> storeyV2(@RequestBody TopicQueryRequest request) {
+        BaseResponse<TopicResponse> response = topicService.detail(request,false);
+
+        this.setTopic13(response,13);         //赋值滚动消息
+
+        return response;
+    }
+
+    private void setTopic13(BaseResponse<TopicResponse> response, int num) {
+        //如果有response有13
+        //List list = null;
+        //response.setList();
+    }
+
 
 }
