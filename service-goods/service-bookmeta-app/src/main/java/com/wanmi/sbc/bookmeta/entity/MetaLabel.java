d6580c63b595ec96db925c97842ffd12181a7ef2
zh <1529960331@qq.com>
2023-02-17 13:34:56 +0800

    标签表

--- service-goods/service-bookmeta-app/src/main/java/com/wanmi/sbc/bookmeta/entity/MetaLabel.java
+++ service-goods/service-bookmeta-app/src/main/java/com/wanmi/sbc/bookmeta/entity/MetaLabel.java
@@ -1,6 +1,5 @@
 package com.wanmi.sbc.bookmeta.entity;
 
-import io.swagger.models.auth.In;
 import lombok.Data;
 
 import java.util.Date;
@@ -87,102 +86,10 @@ public class MetaLabel implements Serializable {
     private Integer isRun;
 
     @Column(name = "run_from_time")
-    private java.sql.Date runFromTime;
+    private Date runFromTime;
 
     @Column(name = "run_to_time")
-    private java.sql.Date runToTime;
-
-    public Integer getIsStatic() {
-        return isStatic;
-    }
-
-    public void setIsStatic(Integer isStatic) {
-        this.isStatic = isStatic;
-    }
-
-    public Integer getIsRun() {
-        return isRun;
-    }
-
-    public void setIsRun(Integer isRun) {
-        this.isRun = isRun;
-    }
-
-    public java.sql.Date getRunFromTime() {
-        return runFromTime;
-    }
-
-    public void setRunFromTime(java.sql.Date runFromTime) {
-        this.runFromTime = runFromTime;
-    }
-
-    public java.sql.Date getRunToTime() {
-        return runToTime;
-    }
-
-    public void setRunToTime(java.sql.Date runToTime) {
-        this.runToTime = runToTime;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Integer getDelFlag() {
-        return delFlag;
-    }
-
-    public void setDelFlag(Integer delFlag) {
-        this.delFlag = delFlag;
-    }
-
-    public String getDescr() {
-        return descr;
-    }
-
-    public void setDescr(String descr) {
-        this.descr = descr;
-    }
-
-    public Integer getSeq() {
-        return seq;
-    }
+    private Date runToTime;
 
     @Column(name = "show_status")
     private Integer showStatus;
