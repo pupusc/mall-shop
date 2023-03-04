1a05e66d2038f85c76eacb347ad6ce81ebb173a8
hanyunke <hanyunke@ditasys.com>
2023-03-04 13:35:32 +0800

    es添加商品标签

--- service-goods/service-goods-app/src/main/java/com/wanmi/sbc/goods/collect/MarketLabel.java
+++ service-goods/service-goods-app/src/main/java/com/wanmi/sbc/goods/collect/MarketLabel.java
@@ -11,7 +11,6 @@ import jodd.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.jvm.hotspot.oops.Mark;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
