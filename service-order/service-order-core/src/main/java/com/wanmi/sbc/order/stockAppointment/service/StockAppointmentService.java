2d131b34ecc7a4e71bf80b5465a20b9ce04da86e
hanyunke <hanyunke@ditasys.com>
2023-03-06 19:01:34 +0800

    预约修改

--- service-order/service-order-core/src/main/java/com/wanmi/sbc/order/stockAppointment/service/StockAppointmentService.java
+++ service-order/service-order-core/src/main/java/com/wanmi/sbc/order/stockAppointment/service/StockAppointmentService.java
@@ -69,10 +69,10 @@ public class StockAppointmentService {
     public BaseResponse deleteById(Integer id) {
         try {
             repository.deleteById(id);
-            return BaseResponse.success("取消预约失败");
+            return BaseResponse.success("取消预约成功");
         }catch (Exception e){
             e.printStackTrace();
-            return BaseResponse.error("取消预约成功");
+            return BaseResponse.error("取消预约失败");
         }
     }
 }
