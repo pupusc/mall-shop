95d0ddb826aecfcacc47021cf3d7c09f3010c56a
pushicheng <pushicheng@ditasys.com>
2023-03-07 16:11:40 +0800

    update SKU

---
+++ service-goods/service-bookmeta-api/src/main/java/com/wanmi/sbc/bookmeta/bo/SkuDetailBO.java
@@ -0,0 +1,24 @@
+package com.wanmi.sbc.bookmeta.bo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: pushicheng
+ * @Date: 2023/03/07/15:11
+ * @Description:
+ */
+@Data
+public class SkuDetailBO implements Serializable {
+    private String skuId;
+    private String img;
+    private String score;
+    private String Isbn;
+    private String saleNum;
+    private String SkuName;
+    private BigDecimal price;
+}
