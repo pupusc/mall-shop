72000594b056556e9aa508f7fbcb359d11b15d68
eddie <1529960331@qq.com>
2023-03-08 14:01:39 +0800

    混合组件

--- platform/service-mobile/src/main/java/com/wanmi/sbc/goodsPool/service/impl/GoodsPoolServiceImpl.java
+++ platform/service-mobile/src/main/java/com/wanmi/sbc/goodsPool/service/impl/GoodsPoolServiceImpl.java
@@ -84,8 +84,9 @@ public class GoodsPoolServiceImpl implements PoolService {
         Integer saleNum = skuDetailBO.getSaleNum() != null ? Integer.valueOf(skuDetailBO.getSaleNum()) : 0;
         String[] digit = {"十+", "百+", "千+", "万+"};
         if (saleNum >= 300) {
-            String num = String.valueOf(saleNum / 100).split(".")[0];
-            score = num.length() <= 3 ? num.substring(0,1) + digit[num.length()] : num.substring(0, num.length()-3) + digit[3];
+            String num = String.valueOf(saleNum / 100);
+            score = num.length() == 1 ? saleNum.toString() : (num.length() == 2 ? num.substring(0,1) + digit[num.length()]
+                    : num.substring(0, num.length()-2) + digit[3]);
         } else {
             //当图书库评分为空取商城商品评分
             score = score != null ? score : skuDetailBO.getScore();
