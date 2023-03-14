a1b64fb0198f4c2473450332ed715eb728b969f9
zh <1529960331@qq.com>
2023-03-14 09:59:50 +0800

    tab标签

---
+++ service-setting/service-setting-bean/src/main/java/com/wanmi/sbc/setting/bean/vo/MetaLabelQueryByPageReqVO.java
@@ -0,0 +1,42 @@
+package com.wanmi.sbc.setting.bean.vo;
+
+import com.wanmi.sbc.common.base.Page;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 标签(MetaLabel)实体类
+ *
+ * @author Liang Jun
+ * @since 2022-05-17 11:38:03
+ */
+@Data
+public class MetaLabelQueryByPageReqVO implements Serializable {
+    private static final long serialVersionUID = -93577520761453533L;
+    
+    private Integer id;
+    /**
+     * 名称
+     */
+    private String name;
+    /**
+     * 状态：1启用；2停用；
+     */
+    private Integer status;
+    /**
+     * 父级id
+     */
+    private Integer parentId;
+    /**
+     * 场景：1适读对象；
+     */
+    private Integer scene;
+    /**
+     * 分页参数
+     */
+    private Page page = new Page(1, 10);
+
+    private Integer isStatic;
+}
+
