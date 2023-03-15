c61726e9e894a2033d4808cee8b98ef0bbd0e148
eddie <1529960331@qq.com>
2023-03-15 17:33:45 +0800

    混合组件

--- platform/service-mobile/src/main/java/com/wanmi/sbc/goodsPool/service/impl/VideoPoolServiceImpl.java
+++ platform/service-mobile/src/main/java/com/wanmi/sbc/goodsPool/service/impl/VideoPoolServiceImpl.java
@@ -152,6 +152,7 @@ public class VideoPoolServiceImpl implements PoolService {
 //                tagsDto.setShowImg(metaLabelBO.getShowImg());
 //                tagsDto.setType(metaLabelBO.getType());
 //                tabs.add(tagsDto);
+
 //            }
 //        }
         //获取标签
