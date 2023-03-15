cd2170670a7891786037bb0a259e2a6fb8dbd673
chenzhen <chenzhen@ditasys.com>
2023-03-15 16:56:31 +0800

    缓存

--- service-goods/service-goods-app/src/main/java/com/wanmi/sbc/goods/provider/impl/common/GoodsRedisController.java
+++ service-goods/service-goods-app/src/main/java/com/wanmi/sbc/goods/provider/impl/common/GoodsRedisController.java
@@ -7,6 +7,7 @@ import com.wanmi.sbc.goods.api.provider.common.RiskVerifyProvider;
 import com.wanmi.sbc.goods.api.request.SuspensionV2.SpuRequest;
 import com.wanmi.sbc.goods.api.request.common.ImageVerifyRequest;
 import com.wanmi.sbc.goods.collect.*;
+import com.wanmi.sbc.goods.collect.respository.GoodRepository;
 import com.wanmi.sbc.goods.common.RiskVerifyService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 @RestController
@@ -30,44 +32,53 @@ public class GoodsRedisController implements GoodsRedisProvider {
     MarketLabel marketLabel;
 
     @Autowired
+    GoodRepository goodRepository;
+
+    @Autowired
     CacheService cacheService;
 
     @Autowired
     GoodsTestCacheService goodsCacheService;
 
-    //单个图书商品isbn查询
+    //通过isbn刷新 卖点标签&&商品详细
+    //ISBN_C_T003
     @Override
     public BaseResponse refreshBook(SpuRequest spuRequest) {
 
         String isbn = spuRequest.getIsbn();
-        String spu_id = spuRequest.getSpu_id();
-        String spu_no = spuRequest.getSpu_no();
 
-        Map map=new HashMap<>();
-
-        map.put("isbn",isbn);
-        map.put("spu_id",spu_id);
-        map.put("spu",spu_no);
-
-        bookTags.doGoods(map);
+        bookTags.doData(isbn);
         System.out.println("isbn"+ isbn);
         return BaseResponse.SUCCESSFUL();
+
     }
 
-    //单个sku商品查询
+    //通过sku_id刷新
+    //sku_id 图书 2c9a009b86a5b1850186a6ae64c80004  spu_id 2c9a00ca86299cda01862a0163e60000
+    //sku_id 非书 2c90c8647c3481e7017c35c181310002  spu_id 2c90c8647c3481e7017c35c181140001
     @Override
     public BaseResponse refreshGoods(SpuRequest spuRequest) {
 
-        String spu_id = spuRequest.getSpu_id();
         String sku_id = spuRequest.getSku_id();
 
-        Map map=new HashMap<>();
+        String spu_id = goodRepository.getSpuIdBySku(sku_id);
 
-        map.put("spu_id",spu_id);//商品spu
-        map.put("sku_id",sku_id);//商品sku
+        String isbn = goodRepository.getIsbnBySpuId(spu_id);
 
-        marketLabel.doData(map);
-        System.out.println("isbn"+ sku_id);
+        if(DitaUtil.isNotBlank(isbn)){
+            bookTags.doData(isbn);          //图书
+        }else{
+            goodTags.doGoods(spu_id);       //非书
+        }
+
+        if(DitaUtil.isNotBlank(spu_id)){
+            Map skuMap = new HashMap();
+            skuMap.put("spu_id",spu_id);
+            skuMap.put("sku_id",sku_id);
+
+            marketLabel.doData(skuMap);
+        }
+
         return BaseResponse.SUCCESSFUL();
     }
 
@@ -89,7 +100,6 @@ public class GoodsRedisController implements GoodsRedisProvider {
 
                     goodsCacheService.clear();
                     cacheService.clear();
-
 
                 }
                 catch (Exception ex) {
