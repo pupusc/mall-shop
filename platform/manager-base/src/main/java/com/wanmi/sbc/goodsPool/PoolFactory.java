83fd30d8b9fc20228b59411d458648675b4c1336
eddie <1529960331@qq.com>
2023-03-15 17:51:04 +0800

    混合组件

--- platform/manager-base/src/main/java/com/wanmi/sbc/goodsPool/PoolFactory.java
+++ platform/manager-base/src/main/java/com/wanmi/sbc/goodsPool/PoolFactory.java
@@ -1,10 +1,10 @@
 package com.wanmi.sbc.goodsPool;
 
 import com.wanmi.sbc.goodsPool.service.PoolService;
-import com.wanmi.sbc.goodsPool.service.impl.AdvertisementPoolServiceImpl;
-import com.wanmi.sbc.goodsPool.service.impl.AssignPoolServiceImpl;
-import com.wanmi.sbc.goodsPool.service.impl.GoodsPoolServiceImpl;
-import com.wanmi.sbc.goodsPool.service.impl.VideoPoolServiceImpl;
+//import com.wanmi.sbc.goodsPool.service.impl.AdvertisementPoolServiceImpl;
+//import com.wanmi.sbc.goodsPool.service.impl.AssignPoolServiceImpl;
+//import com.wanmi.sbc.goodsPool.service.impl.GoodsPoolServiceImpl;
+//import com.wanmi.sbc.goodsPool.service.impl.VideoPoolServiceImpl;
 import com.wanmi.sbc.setting.bean.enums.BookType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ApplicationObjectSupport;
@@ -26,10 +26,10 @@ public class PoolFactory extends ApplicationObjectSupport {
      */
     public PoolService getPoolService(Integer bookType) {
         if (bookType == null) {return null;}
-        if (BookType.BOOK.toValue().equals(bookType)) {return beanFactoryHelper.getBean(GoodsPoolServiceImpl.class);}
-        if (BookType.ADVERTISEMENT.toValue().equals(bookType)) {return beanFactoryHelper.getBean(AdvertisementPoolServiceImpl.class);}
-        if (BookType.VIDEO.toValue().equals(bookType)) {return beanFactoryHelper.getBean(VideoPoolServiceImpl.class);}
-        if (BookType.ASSIGN.toValue().equals(bookType)) {return beanFactoryHelper.getBean(AssignPoolServiceImpl.class);}
+//        if (BookType.BOOK.toValue().equals(bookType)) {return beanFactoryHelper.getBean(GoodsPoolServiceImpl.class);}
+//        if (BookType.ADVERTISEMENT.toValue().equals(bookType)) {return beanFactoryHelper.getBean(AdvertisementPoolServiceImpl.class);}
+//        if (BookType.VIDEO.toValue().equals(bookType)) {return beanFactoryHelper.getBean(VideoPoolServiceImpl.class);}
+//        if (BookType.ASSIGN.toValue().equals(bookType)) {return beanFactoryHelper.getBean(AssignPoolServiceImpl.class);}
         throw new RuntimeException("不存在的商品池类型");
     }
 }
