a3695531e1965fdd7f8d0f7f6a246c7b7b59acfd
chenzhen <chenzhen@ditasys.com>
2023-03-23 10:53:29 +0800

    轮播词

--- service-setting/service-setting-app/src/main/java/com/wanmi/sbc/setting/provider/impl/presetsearch/PresetSearchTermsQueryController.java
+++ service-setting/service-setting-app/src/main/java/com/wanmi/sbc/setting/provider/impl/presetsearch/PresetSearchTermsQueryController.java
@@ -3,8 +3,11 @@ package com.wanmi.sbc.setting.provider.impl.presetsearch;
 
 import com.wanmi.sbc.common.base.BaseResponse;
 import com.wanmi.sbc.setting.api.provider.presetsearch.PresetSearchTermsQueryProvider;
+import com.wanmi.sbc.setting.api.response.preserotation.PresetSearchRotationQueryResponse;
 import com.wanmi.sbc.setting.api.response.presetsearch.PresetSearchTermsQueryResponse;
 import com.wanmi.sbc.setting.presetsearch.service.PresetSearchTermsService;
+import com.wanmi.sbc.setting.searchRotation.model.PresetSearchRotation;
+import com.wanmi.sbc.setting.searchRotation.service.PresetSearchRotationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,9 @@ public class PresetSearchTermsQueryController implements PresetSearchTermsQueryP
     @Autowired
     private PresetSearchTermsService presetSearchTermsService;
 
+    @Autowired
+    private PresetSearchRotationService presetSearchRotationService;
+
     @Override
     public BaseResponse<PresetSearchTermsQueryResponse> listPresetSearchTerms() {
         return BaseResponse.success(presetSearchTermsService.listPresetSearchTerms());
@@ -26,4 +32,11 @@ public class PresetSearchTermsQueryController implements PresetSearchTermsQueryP
     public BaseResponse<PresetSearchTermsQueryResponse> listPresetSearchTermsV2() {
         return BaseResponse.success(presetSearchTermsService.listPresetSearchTermsV2());
     }
+
+    @Override
+    public BaseResponse<PresetSearchRotationQueryResponse> listPresetSearchRotation() {
+        return BaseResponse.success(presetSearchRotationService.findRotation());
+    }
+
+
 }
